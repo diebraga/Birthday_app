d5a4b7635d14053ca4edfd17ef685a3b86d0f752
diebraga <diebraga0055@gmail.com>
2021-10-11 18:12:57 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout'
+import { Button, Center, Heading, Input, VStack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { FormEvent, useEffect, useState } from 'react'
+
+const IndexPage = () => {
+  return (
+    <Layout title="Home">
+      Hello World
+    </Layout>
+  )
+}
+
+export default IndexPage
